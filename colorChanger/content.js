56f4fb956b59ae4f1c8f7371fc7ee0656b822658
Siddharth Batra <sbsiddharth@gmail.com>
2019-06-29 10:11:48 +0530

    changes fonts

--- colorChanger/content.js
+++ colorChanger/content.js
@@ -1,3 +1,30 @@
+let fontOverwrite = `@import url('https://fonts.googleapis.com/css?family=Rubik+Mono+One&display=swap'); * {font-family: 'Rubik Mono One' !important}`;
+let transformOverwrite = "* {text-transform:uppercase}";
+
+// if we don't have the styleDom, create it
+let styleDom = document.getElementById("comic-sans-everything-style");
+if (!styleDom) {
+  styleDom = document
+    .querySelector("head")
+    .appendChild(document.createElement("style"));
+  styleDom.id = "comic-sans-everything-style";
+  styleDom.rel = "stylesheet";
+  styleDom.type = "text/css";
+  console.log(styleDom);
+}
+
+const updateStyle = () => {
+    window.chrome.storage.sync.get(["status"], items => {
+      if (!window.chrome.runtime.error) {
+        if (items.status) {
+          styleDom.innerText = fontOverwrite;
+        } else {
+          styleDom.innerText = "";
+        }
+      }
+    });
+  };
+
 chrome.runtime.sendMessage({todo:"showPageAction"});
 
 chrome.runtime.onMessage.addListener(function(req, sender, res){
@@ -5,7 +32,18 @@ chrome.runtime.onMessage.addListener(function(req, sender, res){
          var colorFinal = '#' + req.clickedColor;
          var size = req.size+'em';
          $(req.selector).css(req.property, colorFinal);
-         $(req.selector).addClass("fontTest");
+         //$(req.selector).addClass("fontTest");
          $(req.selector).css('font-size', size, "important");
+         console.log("hello");
+        var els = document.querySelectorAll('body', 'body *');
+        console.log(els);
+        for(var i = 0; i < els.length; i++){
+            els[i].fontFamily = "Roboto";
+        }
+    }
+    else if(req.todo == "changeFont"){
+        // run once on file load
+        console.log("hello");
+        updateStyle();
     }
 })
--- colorChanger/popup.js
+++ colorChanger/popup.js
@@ -1,4 +1,31 @@
+const setStatus = (status) => {
+  // this might seemed reversed, it is
+  // we want to show the user the opposite of what the current status is
+  if (status === true) {
+    document.getElementById('status').innerHTML = 'On'
+  } else if (status === false) {
+    document.getElementById('status').innerHTML = 'Off'
+  }
+}
+
+window.chrome.storage.sync.get(['status'], (items) => {
+    // if there are no errors starting chrome calls
+    if (!window.chrome.runtime.error) {
+      // if this is the first time the extension starts
+      if (items.status === undefined) {
+        window.chrome.storage.sync.set({ 'status': true })
+        setStatus(true)
+      } else if (items.status) {
+        setStatus(true)
+      } else if (!items.status) {
+        setStatus(false)
+      }
+    }
+  })
+
 $(function(){
+    var els = document.querySelectorAll('body', 'body *');
+    console.log(els);
     var color = $('#bgColor').val();
     var size = parseInt($('#size').text());
     var selector;
@@ -9,6 +36,13 @@ $(function(){
     $('#increase').click(function(){
         size += 1;
         $('#size').text(size);
+        var a = $('body').text();
+        for(var i = 0 ; i < a.length; i++){
+            if(a[i] == 'F'){
+                console.log("found");
+            }
+        }
+        console.log($('body').text());
     })
     $('#decrease').click(function(){
         size -= 1;
@@ -41,5 +75,21 @@ $(function(){
                 chrome.tabs.sendMessage(tabs[0].id, {todo : "changeColor", clickedColor : color, property : property, selector : selector, size : size});
             })
         } 
+    })
+    $('#font_switch').click(function(){
+        window.chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+            let activeTab = tabs[0]
+            window.chrome.storage.sync.get('status', (items) => {
+              if (items.status) {
+                  console.log(items.status);
+                document.getElementById('status').innerText = 'Off'
+              } else {
+                console.log(items.status,"off");
+                document.getElementById('status').innerText = 'On'
+              }
+              window.chrome.storage.sync.set({ 'status': !items.status })
+              window.chrome.tabs.sendMessage(activeTab.id, {'todo' : "changeFont", 'status': !items.status})
+            })
+          })
     })
 })
