dbb497377ab9607186c7549b9aae85a7bf348c22
Siddharth Batra <sbsiddharth@gmail.com>
2019-06-29 12:43:07 +0530

    added full functionality

--- colorChanger/content.js
+++ colorChanger/content.js
@@ -1,4 +1,4 @@
-let fontOverwrite = `@import url('https://fonts.googleapis.com/css?family=Rubik+Mono+One&display=swap'); * {font-family: 'Rubik Mono One' !important}`;
+let fontOverwrite = `@import url('https://cdn.clarkhacks.com/OpenDyslexic/v1/OpenDyslexic.css'); * {font-family: 'OpenDyslexic' !important}`;
 let transformOverwrite = "* {text-transform:uppercase}";
 
 // if we don't have the styleDom, create it
@@ -17,7 +17,7 @@ const updateStyle = () => {
     window.chrome.storage.sync.get(["status"], items => {
       if (!window.chrome.runtime.error) {
         if (items.status) {
-          styleDom.innerText = fontOverwrite;
+          styleDom.innerText += fontOverwrite;
         } else {
           styleDom.innerText = "";
         }
@@ -25,25 +25,88 @@ const updateStyle = () => {
     });
   };
 
+let styleDom2 = document.getElementById("changeBackgroundColor");
+  if (!styleDom2) {
+    styleDom2 = document
+      .querySelector("head")
+      .appendChild(document.createElement("style"));
+    styleDom2.id = "changeBackgroundColor";
+    styleDom2.rel = "stylesheet";
+    styleDom2.type = "text/css";
+    console.log(styleDom);
+  }
+
+let styleDom3 = document.getElementById("changeFontSize");
+  if (!styleDom3) {
+    styleDom3 = document
+      .querySelector("head")
+      .appendChild(document.createElement("style"));
+    styleDom3.id = "changeFontSize";
+    styleDom3.rel = "stylesheet";
+    styleDom3.type = "text/css";
+    console.log(styleDom);
+  }  
+
+const updateColor = (color, override) => {
+    let changeBackground = `body{background-color : ${color}; border-color : ${color}} *{background-color : ${color}}`
+    window.chrome.storage.sync.get(["backgroundStatus"], items => {
+      if (!window.chrome.runtime.error) {
+          console.log("in this")
+          console.log(items.backgroundStatus);
+          console.log(color);
+        if (items.backgroundStatus || override) {
+            styleDom2.innerText = '#'+changeBackground;
+        } else {
+          styleDom2.innerText = "";
+        }
+      }
+    });
+  };
+
+const updateSize = (size, override) => {
+    let changeSize = `body{font-size : ${size}em;}`
+    window.chrome.storage.sync.get(["sizeStatus"], items => {
+      if (!window.chrome.runtime.error) {
+        if (items.sizeStatus || override) {
+            styleDom3.innerText = changeSize;
+        } else {
+          styleDom3.innerText = "";
+        }
+      }
+    });
+}
+
 chrome.runtime.sendMessage({todo:"showPageAction"});
 
 chrome.runtime.onMessage.addListener(function(req, sender, res){
     if(req.todo == "changeColor"){
          var colorFinal = '#' + req.clickedColor;
-         var size = req.size+'em';
-         $(req.selector).css(req.property, colorFinal);
-         //$(req.selector).addClass("fontTest");
-         $(req.selector).css('font-size', size, "important");
-         console.log("hello");
-        var els = document.querySelectorAll('body', 'body *');
-        console.log(els);
-        for(var i = 0; i < els.length; i++){
-            els[i].fontFamily = "Roboto";
-        }
+         updateColor(colorFinal);
     }
     else if(req.todo == "changeFont"){
-        // run once on file load
-        console.log("hello");
+        console.log(req.property);
+        fontOverwrite = `@import url('https://cdn.clarkhacks.com/OpenDyslexic/v1/OpenDyslexic.css'); * {font-family: '${req.property}' !important}`
         updateStyle();
     }
+    else if(req.todo == "changeSize"){
+        var size = req.size;
+        updateSize(size);
+    }
 })
+let storedColor;
+let backgroundOverride = false;
+let sizeOverride = false;
+window.chrome.storage.sync.get(["color"],function(stored){
+    storedColor = '#'+stored.color;
+    console.log(storedColor);
+    window.chrome.storage.sync.get(["backgroundStatus"],function(items){
+        backgroundOverride = items.backgroundStatus;
+        console.log(backgroundOverride);
+    })
+    window.chrome.storage.sync.get(["sizeStatus"],function(items){
+        sizeOverride = items.size;
+    })
+    updateStyle();
+    updateColor(storedColor, backgroundOverride);
+    //updateSize()
+});
--- colorChanger/popup.js
+++ colorChanger/popup.js
@@ -23,6 +23,56 @@ window.chrome.storage.sync.get(['status'], (items) => {
     }
   })
 
+  const setStatus2 = (status) => {
+    // this might seemed reversed, it is
+    // we want to show the user the opposite of what the current status is
+    if (status === true) {
+      document.getElementById('btnStatus').innerHTML = 'On'
+    } else if (status === false) {
+      document.getElementById('btnStatus').innerHTML = 'Off'
+    }
+  }  
+
+  const setStatus3 = (status) => {
+    // this might seemed reversed, it is
+    // we want to show the user the opposite of what the current status is
+    if (status === true) {
+      document.getElementById('sizeStatus').innerHTML = 'On'
+    } else if (status === false) {
+      document.getElementById('sizeStatus').innerHTML = 'Off'
+    }
+  }  
+
+window.chrome.storage.sync.get(['backgroundStatus'], (items) => {
+    // if there are no errors starting chrome calls
+    if (!window.chrome.runtime.error) {
+      // if this is the first time the extension starts
+      if (items.backgroundStatus === undefined) {
+        window.chrome.storage.sync.set({ 'backgroundStatus': true })
+        setStatus2(true)
+      } else if (items.backgroundStatus) {
+        setStatus2(true)
+      } else if (!items.backgroundStatus) {
+        setStatus2(false)
+      }
+    }
+  })
+
+window.chrome.storage.sync.get(['sizeStatus'], (items) => {
+// if there are no errors starting chrome calls
+if (!window.chrome.runtime.error) {
+    // if this is the first time the extension starts
+    if (items.sizeStatus === undefined) {
+    window.chrome.storage.sync.set({ 'sizeStatus': true })
+    setStatus3(true)
+    } else if (items.sizeStatus) {
+    setStatus3(true)
+    } else if (!items.sizeStatus) {
+    setStatus3(false)
+    }
+}
+})
+
 $(function(){
     var els = document.querySelectorAll('body', 'body *');
     console.log(els);
@@ -50,33 +100,48 @@ $(function(){
     })
     $('#bgColor').on('change', function(){
         color = $(this).val();
+        // console.log("changing");
+        // chrome.tabs.query({active : true, currentWindow : true}, function(tabs){
+        //     chrome.tabs.sendMessage(tabs[0].id, {todo : "changeColor", clickedColor : color, property : property, selector : selector, size : size});
+        // })
     })
     $('#btnChange').click(function(){
         $('body').removeClass('minHW');
-        var notifOptions = {
-            type : 'basic',
-            title : 'Name Changed',
-            message : 'you have changed the background color',
-            iconUrl : 'Screenshot (20).png'
-        };
         var n = color.length;
-        // if(n == 6){
-        //     chrome.notifications.create('notif', notifOptions);
-        //     chrome.tabs.query({active : true, currentWindow : true}, function(tabs){
-        //         chrome.tabs.sendMessage(tabs[0].id, {todo : "changeColor", clickedColor : color});
-        //     })
-        // } 
-
         if(n == 6){
-            selector = $('#selectorID').val();
-            property = $('#propertyVal').val();
-            chrome.notifications.create('notif', notifOptions);
+            window.chrome.storage.sync.get('backgroundStatus', (items) => {
+                if (items.backgroundStatus) {
+                  document.getElementById('btnStatus').innerText = 'Off'
+                } else {
+                  document.getElementById('btnStatus').innerText = 'On'
+                }
+            window.chrome.storage.sync.set({ 'backgroundStatus': !items.backgroundStatus })
+            window.chrome.storage.sync.set({ 'color': color })
             chrome.tabs.query({active : true, currentWindow : true}, function(tabs){
-                chrome.tabs.sendMessage(tabs[0].id, {todo : "changeColor", clickedColor : color, property : property, selector : selector, size : size});
+                chrome.tabs.sendMessage(tabs[0].id, {todo : "changeColor", clickedColor : color});
             })
         } 
+    )}
+})
+
+$('#size_change').click(function(){
+    $('body').removeClass('minHW');
+    var n = color.length;
+        window.chrome.storage.sync.get('sizeStatus', (items) => {
+            if (items.sizeStatus) {
+              document.getElementById('sizeStatus').innerText = 'Off'
+            } else {
+              document.getElementById('sizeStatus').innerText = 'On'
+            }
+        window.chrome.storage.sync.set({ 'sizeStaus': !items.sizeStatus })
+        window.chrome.storage.sync.set({ 'size': size })
+        chrome.tabs.query({active : true, currentWindow : true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, {todo : "changeSize", 'size' : size});
+        })
     })
+})
     $('#font_switch').click(function(){
+        property = $('#propertyVal1').val();
         window.chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
             let activeTab = tabs[0]
             window.chrome.storage.sync.get('status', (items) => {
@@ -88,7 +153,7 @@ $(function(){
                 document.getElementById('status').innerText = 'On'
               }
               window.chrome.storage.sync.set({ 'status': !items.status })
-              window.chrome.tabs.sendMessage(activeTab.id, {'todo' : "changeFont", 'status': !items.status})
+              window.chrome.tabs.sendMessage(activeTab.id, {'todo' : "changeFont", 'status': !items.status, property : property})
             })
           })
     })
